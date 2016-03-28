858b86549967002987728105a7902108ee8f9a58
marehr <marehr@zedat.fu-berlin.de>
2016-03-28 23:49:06 +0200

    Export DLLs via VS

--- lib_factorial/factorial.h
+++ lib_factorial/factorial.h
@@ -3,7 +3,16 @@
 
 #include <stdlib.h>
 
+#ifndef _MSC_VER
+#define __shared_export
+#else
+#define __shared_export __declspec(dllexport)
+#endif
+
+__shared_export
 size_t factorial(size_t n);
+
+#undef __shared_export
 
 #endif // #ifndef LIB_HEADER_FACTORIAL_H
 
